c1d2ee86d44fa12ff785a822960ba652fa00cc5e
NhatNguyen1784 <nguyennhatnguyen1782004@gmail.com>
2024-10-08 20:41:09 +0700

    create controller category

---
+++ src/main/java/jpa/vn/controllers/CategoryController.java
@@ -0,0 +1,24 @@
+package jpa.vn.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+public class CategoryController extends HttpServlet implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
